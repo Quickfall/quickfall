bb813f387f3f45dad74021170e0445e0d617f74e
Zffu <radi0o0@outlook.com>
2024-11-28 00:40:42 +0100

    feat: added fastutils header

---
+++ src/utils/fastutils.h
@@ -0,0 +1,12 @@
+/**
+ * Collection of fast functions.
+ */
+
+/**
+ * Compares two strings. If they are equal return 1, if not, return 0.
+int faststrcmp(char* string1, char* string2);
+
+/**
+ * Gets the string "hash" of the provided string.
+ */
+long strhash(char* string);
