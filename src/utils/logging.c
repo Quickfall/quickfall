478da6adef06ee7d86a37590d498242acc0c93de
Zffu <radi0o0@outlook.com>
2024-12-04 00:28:07 +0100

    feat: added showLoggingContext() implementation

--- src/utils/logging.c
+++ src/utils/logging.c
@@ -2,7 +2,7 @@
  * Quickfall Logging Utilities.
  */
 
-#include <stdlib.h>
+#include <stdio.h>
 
 #include "./logging.h"
 
@@ -16,4 +16,14 @@ void logError(char* error) {
 void logWarning(char* warning) {
 	loggingCtx->warnings[loggingCtx->warningCount] = warning;
 	loggingCtx->warningCount++;
+}
+
+void showLogginContext() {
+	for(int i = 0; i < loggingCtx->errorCount; ++i) {
+		printf("%sError: %s%s%s\n", TEXT_HGRAY, TEXT_HRED, loggingCtx->errors[i], RESET);
+	}
+
+	for(int i = 0; i < loggingCtx->warningCount; ++i) {
+		printf("%sWarning: %s%s%s\n", TEXT_HGRAY, TEXT_YELLOW, loggingCtx->warnings[i], RESET);
+	}
 }
