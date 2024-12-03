925a5b37caac190f30a3cf1fab1140b58731eb27
Zffu <radi0o0@outlook.com>
2024-12-04 00:21:14 +0100

    feat: changed loggingCtx to pointer

--- src/utils/logging.c
+++ src/utils/logging.c
@@ -6,20 +6,14 @@
 
 #include "./logging.h"
 
-struct LoggingContext loggingCtx;
-
-loggingCtx.errorCount = 0;
-loggingCtx.warningCount = 0;
-
-loggingCtx.errors = malloc(sizeof(char*) * 100);
-loggingCtx.warnings = malloc(sizeof(char*) * 100);
+struct LoggingContext* loggingCtx;
 
 void logError(char* error) {
-	loggingCtx.errors[loggingCtx.errorCount] = error;
-	loggingCtx.errorCount++;
+	loggingCtx->errors[loggingCtx->errorCount] = error;
+	loggingCtx->errorCount++;
 }
 
 void logWarning(char* warning) {
-	loggingCtx.warning[loggingCtx.warningCount] = warning;
-	loggingCtx.warningCount++;
+	loggingCtx->warnings[loggingCtx->warningCount] = warning;
+	loggingCtx->warningCount++;
 }
