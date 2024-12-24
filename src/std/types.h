2ad03c8433b0d4c06fbc4e0a4765dff92443b80b
Zffu <radi0o0@outlook.com>
2024-12-24 19:51:59 +0100

    feat: added byte equivalent of std types

--- src/std/types.h
+++ src/std/types.h
@@ -15,4 +15,15 @@ typedef enum {
 	BOOL
 } STD_TYPE;
 
+/**
+ * Conversion of the STD_TYPE to thier bytes.
+ * Used for internal and quickfall compiled.
+ */
+unsigned char* STD_TYPES_BYTES[4] = {
+	0x01,
+	0x02,
+	0x03,
+	0x04
+}
+
 #endif
