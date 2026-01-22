63a4e07d5b9c03af4b036dc328a92701b56cde44
Zffu <103074097+Zffu@users.noreply.github.com>
2026-01-22 22:17:45 +0100

    feat: added initial token types

---
+++ lexer/src/token.rs
@@ -0,0 +1,29 @@
+pub enum LexerToken {
+    FUNCTION,
+    VAR,
+    RETURN,
+
+    EQUAL_SIGN,
+
+    SEMICOLON,
+    COMMA,
+    DOT,
+
+    BRACKET_OPEN,
+    BRACKET_CLOSE,
+
+    PAREN_OPEN,
+    PAREN_CLOSE,
+
+    ARRAY_OPEN,
+    ARRAY_CLOSE,
+
+    INT_LIT(i64),
+    STRING_LIT(String),
+
+    ANGEL_BRACKET_OPEN,
+    ANGEL_BRACKET_CLOSE,
+
+    KEYWORD(String, u64),
+    END_OF_FILE
+}
