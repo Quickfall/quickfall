d19e84fc00df1e4c93fd175263ae4110ca7caae4
Zffu <103074097+Zffu@users.noreply.github.com>
2026-01-22 22:29:43 +0100

    feat: added basic parsing

---
+++ lexer/src/lexer.rs
@@ -0,0 +1,126 @@
+use std::{fs, hash::{DefaultHasher, Hash, Hasher}, io::Error};
+
+use crate::token::LexerToken;
+
+const FUNC_KEYWORD_HASH: u64 = 17439195341824537259;
+const RET_KEYWORD_HASH: u64 = 9222097151127739705;
+
+
+pub fn lexer_parse_file(file_path: &String) -> Result<Vec<LexerToken>, Error> {
+    let contents: String = fs::read_to_string(file_path)?;
+    let mut tokens: Vec<LexerToken> = Vec::new();
+
+    let mut i: usize = 0;
+    
+    while i < contents.len() {
+        let c: char = contents.chars().nth(i).unwrap();
+
+        if c.is_numeric() {
+            tokens.push(parse_number_token(&contents, &mut i));
+            continue;
+        }
+
+        if c == '"' {
+            tokens.push(parse_string_token(&contents, &mut i));
+            continue;
+        }
+
+        if c.is_alphabetic() {
+            tokens.push(parse_keyword(&contents, &mut i));
+            continue;
+        }
+
+        i += c.len_utf8();
+
+        match c {
+            '{' => tokens.push(LexerToken::BRACKET_OPEN),
+            '}' => tokens.push(LexerToken::BRACKET_CLOSE),
+            '(' => tokens.push(LexerToken::PAREN_OPEN),
+            ')' => tokens.push(LexerToken::PAREN_CLOSE),
+            '[' => tokens.push(LexerToken::ARRAY_OPEN),
+            ']' => tokens.push(LexerToken::ARRAY_CLOSE),
+            '=' => tokens.push(LexerToken::EQUAL_SIGN),
+            ',' => tokens.push(LexerToken::COMMA),
+            ';' => tokens.push(LexerToken::SEMICOLON),
+            '.' => tokens.push(LexerToken::DOT),
+            '<' => tokens.push(LexerToken::ANGEL_BRACKET_OPEN),
+            '>' => tokens.push(LexerToken::ANGEL_BRACKET_CLOSE),
+            _ => continue
+        }
+
+    }
+
+    tokens.push(LexerToken::END_OF_FILE);
+
+    Ok(tokens)
+}
+
+pub fn parse_number_token(str: &String, ind: &mut usize) -> LexerToken {
+    let start = *ind + 1;
+    let mut end: usize = start;
+    
+    for(i, c) in str[start..].char_indices() {
+        if !c.is_ascii_digit() {
+            break;
+        }
+
+        end = start + i + c.len_utf8();
+    }
+
+    let slice = &str[*ind..end];
+    let num: i64 = slice.parse().expect("The number token should be a number!");
+
+    *ind = end;
+
+    return LexerToken::INT_LIT(num);
+}
+
+fn parse_string_token(str: &String, ind: &mut usize) -> LexerToken {
+    let start = *ind + 1;
+    let mut end: usize = start;
+
+    for(i, c) in str[start..].char_indices() {
+        if c == '"' {
+            end = start + i + c.len_utf8();
+            break;
+        }
+
+        end = start + i + c.len_utf8();
+    }
+
+    let slice = &str[*ind..end];
+
+    *ind = end;
+    
+    return LexerToken::STRING_LIT(slice.to_string());
+}
+
+fn parse_keyword(str: &String, ind: &mut usize) -> LexerToken {
+    let start = *ind;
+    let mut end: usize = start;
+    
+    for(i, c) in str[start..].char_indices() {
+        if !c.is_alphabetic() {
+            break;
+        }
+
+        end = start + i + c.len_utf8();
+    }
+
+    let mut hasher: DefaultHasher = DefaultHasher::new();
+
+    let slice = &str[start..end];
+    slice.hash(&mut hasher);
+
+    let hash: u64 = hasher.finish();
+
+    *ind = end;
+
+    match hash {
+        FUNC_KEYWORD_HASH => return LexerToken::FUNCTION,
+        RET_KEYWORD_HASH => return LexerToken::RETURN,
+        _ => {
+            return LexerToken::KEYWORD(slice.to_string(), hash);
+        }
+    }
+}
