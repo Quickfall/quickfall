407d2f2a883d697cc02362b65f5879659c179938
Zffu <103074097+Zffu@users.noreply.github.com>
2026-01-22 22:37:57 +0100

    feat: added lexer parsing errors

--- lexer/src/lib.rs
+++ lexer/src/lib.rs
@@ -1,2 +1,25 @@
+use core::fmt;
+
 pub mod token;
 pub mod lexer;
+
+type LexerParseResult<T> = std::result::Result<T, LexerParsingError>;
+
+#[derive(Debug, Clone)]
+pub struct LexerParsingError {
+    reason: String,
+
+    position: usize
+}
+
+impl LexerParsingError {
+    pub fn new(reason: String, pos: usize) -> Self {
+        LexerParsingError { reason, position: pos }
+    }
+}
+
+impl fmt::Display for LexerParsingError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Lexer Parsing Error: {} at position {}", self.reason, self.position)
+    }
+}
