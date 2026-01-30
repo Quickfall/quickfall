cfe13e67e17aecf7364097b11ae8cb83b07167d6
Zffu <103074097+Zffu@users.noreply.github.com>
2026-01-31 00:05:01 +0100

    feat: added var parsing

--- parser/src/ast/mod.rs
+++ parser/src/ast/mod.rs
@@ -10,15 +10,20 @@ use std::fmt::Debug;
 
 use lexer::token::LexerToken;
 
-use crate::{ParserError, ParserResult, ast::{func::decl::parse_function_declaraction, tree::ASTTreeNode}};
+use crate::{ParserError, ParserResult, ast::{func::decl::parse_function_declaraction, tree::ASTTreeNode, var::decl::parse_variable_declaration}};
 
 pub mod tree;
 pub mod func;
+pub mod var;
 
 pub fn parse_ast_node(tokens: &Vec<LexerToken>, ind: &mut usize) -> ParserResult<Box<ASTTreeNode>> {
 	match &tokens[*ind] {
 		LexerToken::FUNCTION => {
 			return parse_function_declaraction(tokens, ind);
+		}
+
+		LexerToken::VAR => {
+			return parse_variable_declaration(tokens, ind);
 		}
 
 		_ => {
---
+++ parser/src/ast/var/decl.rs
@@ -0,0 +1,33 @@
+use lexer::token::LexerToken;
+use utils::hash::WithHash;
+
+use crate::{ParserError, ParserResult, ast::{parse_ast_node, tree::ASTTreeNode}};
+
+pub fn parse_variable_declaration(tokens: &Vec<LexerToken>, ind: &mut usize) -> ParserResult<Box<ASTTreeNode>> {
+	*ind += 1;
+
+	let typeName = match tokens[*ind].as_keyword() {
+		Ok(val) => val,
+		Err(e) => return Err(ParserError::new(String::from("Type name isn't a keyword"), 0))
+	};
+
+	*ind += 1;
+
+	let varName = match tokens[*ind].as_keyword() {
+		Ok(val) => val,
+		Err(e) => return Err(ParserError::new(String::from("Var name isn't a keyword!"), 0))
+	};
+
+	*ind += 1;
+
+	let mut val: Option<Box<ASTTreeNode>> = None;
+
+	if tokens[*ind] == LexerToken::EQUAL_SIGN {
+		val = match parse_ast_node(tokens, ind) {
+			Ok(v) => Some(v),
+			Err(e) => None
+		};
+	}
+
+	return Ok(Box::new(ASTTreeNode::VarDeclaration { varName: WithHash::new(varName.0), varType: typeName.1, value: val }));
+}
