155e35bddec75b610840a18c24be96d6f2b09bbc
Zffu <103074097+Zffu@users.noreply.github.com>
2026-02-15 00:04:01 +0100

    feat: added ptr utils to ir

---
+++ ir/src/irstruct/mod.rs
@@ -0,0 +1,3 @@
+//! Common definitions used internally to structure the IR.
+
+pub mod ptr;
---
+++ ir/src/irstruct/ptr.rs
@@ -0,0 +1,32 @@
+use commons::err::{PositionlessError, PositionlessResult};
+use inkwell::{builder::Builder, values::{BasicValue, IntValue, PointerValue}};
+
+use crate::types::typing::IRType;
+
+pub struct IRPointer<'a> {
+	inkwell_ptr: PointerValue<'a>,
+	t: &'a IRType<'a>,
+	pub name: String
+}
+
+impl<'a> IRPointer<'a> {
+	pub fn new(ptr: PointerValue<'a>, t: &'a IRType<'a>, name: String) -> Self {
+		return IRPointer { inkwell_ptr: ptr, name, t }
+	}
+
+	pub fn load_val_int(&self, builder: &Builder<'a>) -> PositionlessResult<IntValue<'a>> {
+		if !self.t.is_numeric_type() {
+			return Err(PositionlessError::new("Requires a numeric type!"));
+		}
+
+		match builder.build_load(*self.t.get_inkwell_inttype()?, self.inkwell_ptr, &self.name) {
+			Ok(v) => return Ok(v.into_int_value()),
+			Err(_) => return Err(PositionlessError::new("build_load failed!"))
+		}
+	} 
+
+	pub fn store<V: BasicValue<'a>>(&self, builder: &Builder<'a>, val: V) {
+		builder.build_store(self.inkwell_ptr, val);
+	}
+
+}
