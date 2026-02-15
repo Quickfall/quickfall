dacd69bb8c22972746ddbaedb03dc40a0d6928da
Zffu <103074097+Zffu@users.noreply.github.com>
2026-02-16 00:06:27 +0100

    feat: added compiletime replacement for integer based types on global vars

--- ir/src/irstruct/staticvars.rs
+++ ir/src/irstruct/staticvars.rs
@@ -1,12 +1,13 @@
 //! Static variable related code
 
 use commons::err::{PositionlessError, PositionlessResult};
-use inkwell::{builder::Builder, context::Context, values::GlobalValue};
+use inkwell::{builder::Builder, context::Context, types::StringRadix, values::{BasicValueEnum, GlobalValue, IntValue}};
 
 use crate::types::typing::IRType;
 
 pub struct IRStaticVariable<'a> {
 	inkwell: Option<GlobalValue<'a>>,
+	val: Option<BasicValueEnum<'a>>,
 	t: &'a IRType<'a>,
 	pub name: String
 }
@@ -18,7 +19,40 @@ impl<'a> IRStaticVariable<'a> {
 			Err(_) => return Err(PositionlessError::new("build_global_string_ptr failed!!"))
 		};
 
-		return Ok(IRStaticVariable { inkwell: Some(inkwell), t, name });
+		return Ok(IRStaticVariable { inkwell: Some(inkwell), t, name, val: None });
+	}
+
+	pub fn from_int(t: &'a IRType<'a>, name: String, val: i128) -> PositionlessResult<IRStaticVariable<'a>> {
+		let val = match t.get_inkwell_inttype()?.const_int_from_string(&val.to_string(), StringRadix::Decimal) {
+			Some(v) => v,
+			None => return Err(PositionlessError::new("const_int_from_string failed!!"))
+		};
+
+		return Ok(IRStaticVariable { inkwell: None, val: Some(val.into()), t, name })
+	}
+
+	pub fn is_compiletime_replaceable(&self) -> bool {
+		return self.val.is_some();
+	}
+
+	pub fn as_int_val(&self) -> PositionlessResult<IntValue<'a>> {
+		if !self.is_compiletime_replaceable() {
+			return Err(PositionlessError::new("Tried using as_int_val on a non-compiletime determined global val"));
+		}
+
+		if !self.t.is_numeric_type() {
+			return Err(PositionlessError::new("Tried using as_int_val on a non-integer global value type!"));
+		}
+
+		return Ok(self.val.unwrap().into_int_value());
+	}
+
+	pub fn as_string_ref(&self) -> PositionlessResult<GlobalValue<'a>> {
+		if self.is_compiletime_replaceable() {
+			return Err(PositionlessError::new("Tried using as_string_ref on a compiletime determined global var"));
+		}
+
+		return Ok(self.inkwell.unwrap())
 	}
 
 }
