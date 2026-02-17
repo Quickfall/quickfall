15a3fc12efc23406ebd1ce847f386079abd26395
Zffu <103074097+Zffu@users.noreply.github.com>
2026-02-17 17:07:28 +0100

    feat: started working on passing IRContext on every function that needs it

--- ir/src/ctx.rs
+++ ir/src/ctx.rs
@@ -1,7 +1,9 @@
 //! IR context related code
 
+use std::ops::Add;
+
 use commons::utils::map::HashedMap;
-use inkwell::context::Context;
+use inkwell::{AddressSpace, builder::Builder, context::Context, types::PointerType};
 
 use crate::irstruct::{funcs::IRFunction, ptr::IRPointer};
 
@@ -9,7 +11,15 @@ use crate::irstruct::{funcs::IRFunction, ptr::IRPointer};
 /// Basically holds anything related to the current IR compilation (eg: functions, types, global vars)
 pub struct IRContext<'a> {
 	pub inkwell_ctx: &'a Context,
+	pub builder: Builder<'a>,
+	pub ptr_type: PointerType<'a>,
+
 	pub functions: HashedMap<IRFunction<'a>>,
 	pub static_vars: HashedMap<IRPointer<'a>>
+}
 
+impl<'a> IRContext<'a> {
+	pub fn new(builder: Builder<'a>, ctx: &'a Context) -> Self {
+		return IRContext { inkwell_ctx: ctx, builder, ptr_type: ctx.ptr_type(AddressSpace::from(0)), functions: HashedMap::new(0), static_vars: HashedMap::new(0) }
+	}
 }
