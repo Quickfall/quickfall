1bef895aaa2dae354735306273a758bcd173a5bf
Zffu <103074097+Zffu@users.noreply.github.com>
2026-02-18 18:55:09 +0100

    feat: added bool comparing creation

--- ir/src/bools.rs
+++ ir/src/bools.rs
@@ -1,6 +1,8 @@
 //! Everything related to boolean handling and generation
 
 use commons::err::{PositionlessError, PositionlessResult};
+use inkwell::IntPredicate;
+use lexer::toks::comp::ComparingOperator;
 
 use crate::{ctx::IRContext, refs::IRValueRef, types::BOOL_TYPE_HASH, values::IRValue};
 
@@ -24,4 +26,55 @@ pub fn make_bool_xor<'a>(ctx: &'a IRContext<'a>, b: IRValueRef<'a>) -> Positionl
 	};
 
 	return Ok(IRValue::new(xor_v.into(), bool_t));
+}
+
+pub fn make_bool_cmp_int<'a>(ctx: &'a IRContext<'a>, a: IRValueRef<'a>, b: IRValueRef<'a>, comp: ComparingOperator) -> PositionlessResult<IRValue<'a>> {
+	let a_val = a.obtain(ctx)?;
+	let b_val = b.obtain(ctx)?;
+
+	let t = a_val.t;
+
+	let a_int = match a_val.obtain_as_int(t) {
+		Some(v) => v,
+		None => return Err(PositionlessError::new("Value a wasn't an int when trying to use comp!"))
+	};
+
+	let b_int = match b_val.obtain_as_int(t) {
+		Some(v) => v,
+		None => return Err(PositionlessError::new("Value b wasn't an int when trying to use comp!"))
+	};
+
+	let predicate: IntPredicate;
+
+	if t.is_signed() {
+		predicate = match comp {
+			ComparingOperator::Equal => IntPredicate::EQ,
+			ComparingOperator::NotEqual => IntPredicate::NE,
+			ComparingOperator::Higher => IntPredicate::SGT,
+			ComparingOperator::HigherEqual => IntPredicate::SGE,
+			ComparingOperator::Lower => IntPredicate::SLT,
+			ComparingOperator::LowerEqual => IntPredicate::SLE
+		};
+	} else {
+		predicate = match comp {
+			ComparingOperator::Equal => IntPredicate::EQ,
+			ComparingOperator::NotEqual => IntPredicate::NE,
+			ComparingOperator::Higher => IntPredicate::ULT,
+			ComparingOperator::HigherEqual => IntPredicate::UGT,
+			ComparingOperator::Lower => IntPredicate::ULE,
+			ComparingOperator::LowerEqual => IntPredicate::UGE
+		}
+	}
+
+	let cmp = match ctx.builder.build_int_compare(predicate, a_int, b_int, "_cmp") {
+		Ok(v) => v,
+		Err(_) => return Err(PositionlessError::new("build_int_compare failed!"))
+	};
+
+	let bool_t = match ctx.type_storage.get(BOOL_TYPE_HASH) {
+		Some(v) => v,
+		None => return Err(PositionlessError::new("boolean type wasn't found!"))
+	};
+
+	return Ok(IRValue::new(cmp.into(), bool_t));
 }
