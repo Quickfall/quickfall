77acd8fac669a190404fbacbd75686e4b852ef5d
Zffu <103074097+Zffu@users.noreply.github.com>
2026-02-19 21:38:19 +0100

    feat: added new sized error position

---
+++ errors/src/pos.rs
@@ -0,0 +1,28 @@
+use std::{fs, io};
+
+pub struct ErrorPosition {
+	pub line: usize,
+
+	pub col: usize,
+	pub size: usize,
+
+	pub file_path: String
+}
+
+impl ErrorPosition {
+	pub fn new(path: String, line: usize, col: usize, end: usize) -> Self {
+		return ErrorPosition { line, col, size: end - col, file_path: path }
+	}
+
+	pub fn get_line_str(&self) -> Result<String, io::Error> {
+		let contents = fs::read_to_string(&self.file_path)?;
+
+		let split: Vec<&str> = contents.split("\n").collect();
+
+		return Ok(split[self.line].to_string())
+	}
+
+	pub fn get_line_slice(&self) -> Result<String, io::Error> {
+		return Ok(self.get_line_str()?[self.col - 1..self.col + self.size - 1].to_string());
+	}
+}
