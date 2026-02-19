28987b3abb3f5a7535656e4ef170381190dcacc1
Zffu <103074097+Zffu@users.noreply.github.com>
2026-02-19 10:09:33 +0100

    feat: added for loop parsing

--- ir/src/conv/control.rs
+++ ir/src/conv/control.rs
@@ -3,7 +3,7 @@ use std::sync::Condvar;
 use commons::err::{PositionlessError, PositionlessResult};
 use parser::ast::tree::ASTTreeNode;
 
-use crate::{conv::{func::parse_ir_body, val::parse_ir_value}, ctx::{IRContext, IRLocalContext}, irstruct::funcs::IRFunction, types::BOOL_TYPE_HASH};
+use crate::{conv::{self, func::{parse_ir_body, parse_ir_function_body_member}, val::parse_ir_value}, ctx::{IRContext, IRLocalContext}, irstruct::funcs::IRFunction, types::BOOL_TYPE_HASH};
 
 pub fn parse_if_statement_ir(func: &mut IRFunction, ctx: &IRContext, node: Box<ASTTreeNode>) -> PositionlessResult<bool> {
 	if let ASTTreeNode::IfStatement { cond, body, branches, depth } = *node {
@@ -103,5 +103,33 @@ pub fn parse_if_statement_ir(func: &mut IRFunction, ctx: &IRContext, node: Box<A
 }
 
 pub fn parse_for_statement_ir(func: &mut IRFunction, ctx: &IRContext, node: Box<ASTTreeNode>) -> PositionlessResult<bool> {
+	if let ASTTreeNode::ForBlock { initial_state, cond, increment, body } = *node {
+		let for_cond_block = ctx.inkwell_ctx.append_basic_block(func.inkwell_func, "for_cond");
+		let for_body_block = ctx.inkwell_ctx.append_basic_block(func.inkwell_func, "for_inner");
+		let post_block = ctx.inkwell_ctx.append_basic_block(func.inkwell_func, "for_out");
+
+		parse_ir_function_body_member(ctx, func, initial_state)?;
+
+		ctx.builder.build_unconditional_branch(for_cond_block);
+
+		ctx.builder.position_at_end(for_cond_block);
+
+		let bool_type = ctx.type_storage.get(BOOL_TYPE_HASH).expect("Boolean type wasn't found!");
+
+		let cond_val = parse_ir_value(Some(&func.lctx), ctx, cond, None)?;
+		let cond_int = cond_val.obtain(ctx)?.obtain_as_int(ctx, bool_type).expect("Cannot cast condition result as int");
+
+		ctx.builder.build_conditional_branch(*cond_int, for_body_block, post_block);
+		
+		ctx.builder.position_at_end(for_body_block);
+
+		parse_ir_body(ctx, func, body)?;
+
+		parse_ir_function_body_member(ctx, func, increment)?;
+
+		ctx.builder.position_at_end(post_block);
+	}
+
+
 	return Err(PositionlessError::new("Cannot parse for statement as this is not an for!"));
 }
