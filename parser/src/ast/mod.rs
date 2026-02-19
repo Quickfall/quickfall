68e596ab64d1578ed6f3f00797a0d56b8038b77c
Zffu <103074097+Zffu@users.noreply.github.com>
2026-02-19 14:10:25 +0100

    feat: added shadow function declaration parsing

--- parser/src/ast/mod.rs
+++ parser/src/ast/mod.rs
@@ -21,7 +21,7 @@ use commons::err::PositionedResult;
 use lexer::token::{LexerToken, LexerTokenType};
 use utils::hash::WithHash;
 
-use crate::ast::{control::{forloop::parse_for_loop, ifelse::parse_if_statement, whileblock::parse_while_block}, func::{call::parse_function_call, decl::parse_function_declaraction, parse_function_return_statement}, literals::{parse_integer_literal, parse_string_literal}, math::parse_math_operation, tree::ASTTreeNode, types::parse_type_declaration, var::{decl::parse_variable_declaration, staticdecl::parse_static_function_declaration}};
+use crate::ast::{control::{forloop::parse_for_loop, ifelse::parse_if_statement, whileblock::parse_while_block}, func::{call::parse_function_call, decl::{parse_function_declaraction, parse_shadow_function_declaration}, parse_function_return_statement}, literals::{parse_integer_literal, parse_string_literal}, math::parse_math_operation, tree::ASTTreeNode, types::parse_type_declaration, var::{decl::parse_variable_declaration, staticdecl::parse_static_function_declaration}};
 
 pub mod tree;
 pub mod func;
@@ -195,6 +195,10 @@ pub fn parse_ast_node(tokens: &Vec<LexerToken>, ind: &mut usize) -> PositionedRe
 		LexerTokenType::Function => {
 			return parse_function_declaraction(tokens, ind);
 		},
+
+		LexerTokenType::ShadowFunction => {
+			return parse_shadow_function_declaration(tokens, ind);
+		}
 
 		LexerTokenType::Struct => {
 			return parse_type_declaration(tokens, ind, false);
