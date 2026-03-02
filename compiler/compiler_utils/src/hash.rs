32acd9ff8c39e2004bf990912ab9a1f7a92aa878
Zffu <103074097+Zffu@users.noreply.github.com>
2026-03-03 00:38:20 +0100

    feat: added compile-time hashes for types

--- compiler/compiler_utils/src/hash.rs
+++ compiler/compiler_utils/src/hash.rs
@@ -85,13 +85,6 @@ fn utils_get_hash<K: Hash>(val: &K) -> TypeHash {
     hasher.finish()
 }   
 
-#[macro_export]
-macro_rules! hash {
-	($e:expr) => {
-		WithHash::new($e)
-	};
-}
-
 #[derive(Eq, PartialEq)]
 pub struct SelfHash {
 	pub hash: u64
@@ -101,4 +94,25 @@ impl Hash for SelfHash {
 	fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
 		state.write_u64(self.hash);
 	}
+}
+
+pub const fn fnv_1ahash(to_hash: &str) -> u64 {
+	let mut hash: u64 = 14695981039346656037;
+	let bytes = to_hash.as_bytes();
+	let mut i = 0;
+
+	while i < bytes.len() {
+		hash ^= bytes[i] as u64;
+		hash = hash.wrapping_mul(1099511628211);
+		i += 1;
+	}
+
+	return hash;
+} 
+
+#[macro_export]
+macro_rules! hash {
+	($lit:literal) => {
+		compiler_utils::hash::fnv_1ahash($lit)
+	};
 }
