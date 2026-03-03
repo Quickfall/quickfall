26b0748cb4f6eec092aa14e95353f815dfbf8096
Zffu <103074097+Zffu@users.noreply.github.com>
2026-03-03 17:25:36 +0100

    feat: added StructTypeContainer

--- compiler/astoir_hir/src/lib.rs
+++ compiler/astoir_hir/src/lib.rs
@@ -1,5 +1,6 @@
-//! The HIR layer of the AstoIR. T
-//! he HIR layer represents a near AST where elements are resolved instead of raw name hashes, enforcing that the code works in theory.
+//! The HIR layer of the AstoIR. 
+//! The HIR layer represents a near AST where elements are resolved instead of raw name hashes, enforcing that the code works in theory.
 
 pub mod ctx;
 pub mod nodes;
+pub mod structs;
---
+++ compiler/astoir_hir/src/structs.rs
@@ -0,0 +1,35 @@
+//! AstoIR HIR structures related to HIR nodes
+
+use std::collections::HashMap;
+
+use astoir_typing::complete::CompleteType;
+use compiler_errors::{IR_FIELD, errs::{BaseResult, CompilerResult, base::BaseError, normal::CompilerError}};
+use compiler_utils::hash::SelfHash;
+
+pub struct StructTypeContainer {
+	pub hash_to_ind: HashMap<SelfHash, usize>,
+	pub fields: Vec<CompleteType>
+}
+
+impl StructTypeContainer {
+	pub fn new() -> Self {
+		return StructTypeContainer { hash_to_ind: HashMap::new(), fields: vec![] }
+	}
+
+	pub fn append(&mut self, hash: u64, v: CompleteType) {
+		let sz = self.fields.len();
+
+		self.hash_to_ind.insert(SelfHash { hash }, sz);
+		self.fields.push(v);
+	}
+
+	pub fn get(&mut self, hash: u64) -> BaseResult<&CompleteType> {
+		let index = match self.hash_to_ind.get(&SelfHash { hash }) {
+			Some(v) => *v,
+			None => return Err(BaseError::err(IR_FIELD!().to_string()))
+		};
+
+		return Ok(&self.fields[index])
+	}
+
+}
