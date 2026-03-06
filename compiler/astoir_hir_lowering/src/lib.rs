936146328df9c7099d31b4f81e9d843f3facff2a
Zffu <103074097+Zffu@users.noreply.github.com>
2026-03-06 09:57:45 +0100

    feat: added math operator support in ast body lowering

--- compiler/astoir_hir_lowering/src/lib.rs
+++ compiler/astoir_hir_lowering/src/lib.rs
@@ -2,7 +2,7 @@ use ast::tree::{ASTTreeNode, ASTTreeNodeKind};
 use astoir_hir::{ctx::{HIRBranchedContext, HIRContext}, nodes::HIRNode};
 use compiler_errors::{IR_INVALID_NODE_TYPE, errs::{CompilerResult, ErrorKind, normal::CompilerError}};
 
-use crate::{func::lower_ast_function_call, var::lower_ast_variable_declaration};
+use crate::{func::lower_ast_function_call, math::lower_ast_math_operation, var::lower_ast_variable_declaration};
 
 pub mod literals;
 pub mod var;
@@ -15,6 +15,7 @@ pub fn lower_ast_body_node(context: &HIRContext, curr_ctx: &mut HIRBranchedConte
 	match node.kind {
 		ASTTreeNodeKind::VarDeclaration { .. } => return lower_ast_variable_declaration(context, curr_ctx, node),
 		ASTTreeNodeKind::FunctionCall { .. } => return lower_ast_function_call(context, curr_ctx, node),
+		ASTTreeNodeKind::MathResult { .. } => return lower_ast_math_operation(context, curr_ctx, node, true),
 		
 		_ => return Err(CompilerError::from_ast(ErrorKind::Error, IR_INVALID_NODE_TYPE!().to_string(), &node.start, &node.end))
 	}
