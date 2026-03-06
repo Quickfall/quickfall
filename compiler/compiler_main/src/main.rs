c3551794041585bd5ccec0333278b46b6173f3d5
Zffu <103074097+Zffu@users.noreply.github.com>
2026-03-06 14:45:04 +0100

    feat: started working on simple cli

--- compiler/compiler_main/src/main.rs
+++ compiler/compiler_main/src/main.rs
@@ -1,3 +1,21 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+	let arguments: Vec<String> = env::args().collect();
+
+	if arguments.is_empty() {
+		println!("Usage: quickfall comp|astoir");
+		return;
+	}
+
+	match &arguments[0] as &str {
+		"astoir" => {
+			handle
+		},
+
+		_ => {
+			println!("Invalid subcommand!");
+			return;
+		}
+	}
 }
