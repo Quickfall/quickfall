4063f0beb178ca8152db962df072b7d0848ffe18
Zffu <103074097+Zffu@users.noreply.github.com>
2026-03-06 09:52:19 +0100

    feat: added function call lowering in tree parsing

--- compiler/astoir_hir_lowering/src/lib.rs
+++ compiler/astoir_hir_lowering/src/lib.rs
@@ -2,7 +2,7 @@ use ast::tree::{ASTTreeNode, ASTTreeNodeKind};
 use astoir_hir::{ctx::{HIRBranchedContext, HIRContext}, nodes::HIRNode};
 use compiler_errors::{IR_INVALID_NODE_TYPE, errs::{CompilerResult, ErrorKind, normal::CompilerError}};
 
-use crate::var::lower_ast_variable_declaration;
+use crate::{func::lower_ast_function_call, var::lower_ast_variable_declaration};
 
 pub mod literals;
 pub mod var;
@@ -13,6 +13,7 @@ pub mod func;
 pub fn lower_ast_body_node(context: &HIRContext, curr_ctx: &mut HIRBranchedContext, node: Box<ASTTreeNode>) -> CompilerResult<Box<HIRNode>> {
 	match node.kind {
 		ASTTreeNodeKind::VarDeclaration { .. } => return lower_ast_variable_declaration(context, curr_ctx, node),
+		ASTTreeNodeKind::FunctionCall { .. } => return lower_ast_function_call(context, curr_ctx, node),
 		
 		_ => return Err(CompilerError::from_ast(ErrorKind::Error, IR_INVALID_NODE_TYPE!().to_string(), &node.start, &node.end))
 	}
