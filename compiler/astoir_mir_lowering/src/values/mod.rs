e0471475760f5e41a2ab0900c16db24da814a9aa
Zffu <103074097+Zffu@users.noreply.github.com>
2026-03-11 23:10:32 +0100

    feat: added basic mir lowering for values

--- compiler/astoir_mir_lowering/src/values/mod.rs
+++ compiler/astoir_mir_lowering/src/values/mod.rs
@@ -1,1 +1,16 @@
+use astoir_hir::{ctx::HIRContext, nodes::HIRNode};
+use astoir_mir::{blocks::MIRBlock, vals::base::BaseMIRValue};
+use compiler_errors::{IR_INVALID_NODE_TYPE, errs::{BaseResult, base::BaseError}};
+
+use crate::{values::consts::lower_hir_literal, vars::lower_hir_variable_reference_value};
+
 pub mod consts;
+
+pub fn lower_hir_value(block: &mut MIRBlock, node: Box<HIRNode>, ctx: &HIRContext) -> BaseResult<BaseMIRValue> {
+	match *node {
+		HIRNode::IntegerLiteral { .. } | HIRNode::StringLiteral { .. } => return lower_hir_literal(block, ctx, node),
+		HIRNode::VariableReference { .. } => return lower_hir_variable_reference_value(block, node),
+
+		_ => return Err(BaseError::err(IR_INVALID_NODE_TYPE!().to_string()))
+	}
+}
