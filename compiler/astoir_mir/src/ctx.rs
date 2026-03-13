7c3a4072ebf40725f2ee06e6cc670a0575c2afe0
Zffu <103074097+Zffu@users.noreply.github.com>
2026-03-13 12:23:19 +0100

    feat: added global MIR context

--- compiler/astoir_mir/src/ctx.rs
+++ compiler/astoir_mir/src/ctx.rs
@@ -1,4 +1,15 @@
-use crate::vals::ptr::MIRPointerValue;
+use crate::{funcs::MIRFunction, vals::ptr::MIRPointerValue};
+
+
+pub struct MIRContext {
+	pub functions: Vec<MIRFunction>
+}
+
+impl MIRContext {
+	pub fn new() -> Self {
+		MIRContext { functions: vec![] }
+	}
+}
 
 pub struct MIRBlockContext {
 	/// Converts the raw HIR variable indexes into actual pointer values
