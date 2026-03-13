bc72dd0b95ed0798cb6523d91656c874bde78274
Zffu <103074097+Zffu@users.noreply.github.com>
2026-03-13 12:10:54 +0100

    feat: added float operation support

--- compiler/astoir_mir_lowering/src/math.rs
+++ compiler/astoir_mir_lowering/src/math.rs
@@ -1,5 +1,5 @@
 use astoir_hir::{ctx::HIRContext, nodes::HIRNode};
-use astoir_mir::{blocks::MIRBlock, builder::{build_float_add, build_int_add, build_int_div, build_int_mul, build_int_sub, build_store}, vals::base::BaseMIRValue};
+use astoir_mir::{blocks::MIRBlock, builder::{build_float_add, build_float_div, build_float_mul, build_float_sub, build_int_add, build_int_div, build_int_mul, build_int_sub, build_store}, ctx, vals::base::BaseMIRValue};
 use astoir_typing::base::BaseType;
 use compiler_errors::{IR_INVALID_NODE_TYPE, IR_REQ_VARIABLE_ASSIGN, errs::{BaseResult, base::BaseError}};
 use lexer::toks::math::MathOperator;
@@ -26,6 +26,7 @@ pub fn lower_hir_math_operation(block: &mut MIRBlock, node: Box<HIRNode>, ctx: &
 
 		let val = match left_val.vtype.base {
 			BaseType::NumericIntegerType(_, _) => lower_hir_math_operation_int(block, left_val, right_val, operation, ctx)?,
+			BaseType::FloatingNumberType(_, _, _) => lower_hir_math_operation_float(block, left_val, right_val, operation, ctx)?,
 
 			_ => return Err(BaseError::err("Cannot use lower_hir_math_operation on this given value kind!".to_string()))
 		};
@@ -49,6 +50,22 @@ pub fn lower_hir_math_operation_int(block: &mut MIRBlock, left: BaseMIRValue, ri
 		MathOperator::SUBSTRACT => build_int_sub(block, left, right, signed)?,
 		MathOperator::MULTIPLY => build_int_mul(block, left, right, signed)?,
 		MathOperator::DIVIDE => build_int_div(block, left, right, signed)?
+	};
+
+	return Ok(res.into());
+}
+
+pub fn lower_hir_math_operation_float(block: &mut MIRBlock, left: BaseMIRValue, right: BaseMIRValue, operator: MathOperator, ctx: &HIRContext) -> BaseResult<BaseMIRValue> {
+	let left = left.as_float()?;
+	let right = right.as_float()?;
+
+	let signed = left.signed;
+
+	let res = match operator {
+		MathOperator::ADD => build_float_add(block, left, right, signed)?,
+		MathOperator::SUBSTRACT => build_float_sub(block, left, right, signed)?,
+		MathOperator::MULTIPLY => build_float_mul(block, left, right, signed)?,
+		MathOperator::DIVIDE => build_float_div(block, left, right, signed)?
 	};
 
 	return Ok(res.into());
