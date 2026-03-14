c77fcbd957458cb3f763f3a429b943fe7e5999a2
Zffu <103074097+Zffu@users.noreply.github.com>
2026-03-14 23:50:08 +0100

    feat: added variable kinds

--- compiler/astoir_mir/src/blocks/mod.rs
+++ compiler/astoir_mir/src/blocks/mod.rs
@@ -3,12 +3,25 @@ use crate::{blocks::hints::HintStorage, ctx::{MIRBlockContext, MIRContext}, inst
 pub mod refer;
 pub mod hints;
 
+/// The type of variable inside of a MIR block.
+pub enum MIRBlockVariableType {
+	/// SSAs, allow for direct register usage.
+	/// Requires:
+	/// - Variable's address not being obtained (value never referenced)
+	SSA,
+
+	/// Pointer value, uses the stack. 
+	/// Should be used incase SSA fails.
+	Pointer
+}
+
 /// Represents a function block or a branch.
 pub struct MIRBlock {
 	instructions: Vec<MIRInstruction>,
 
 	/// Hints for the index of the SSA value for the given variable. Will be the pointer value if the variable is not SSA.
 	pub variable_hints: Vec<usize>, 
+	pub variable_kinds: Vec<MIRBlockVariableType>,
 
 	pub hints: HintStorage,
 	pub ctx: MIRBlockContext
@@ -16,7 +29,7 @@ pub struct MIRBlock {
 
 impl MIRBlock {
 	pub fn new() -> Self {
-		MIRBlock { instructions: vec![], variable_hints: vec![], hints: HintStorage::new(), ctx: MIRBlockContext::new() }
+		MIRBlock { instructions: vec![], variable_hints: vec![], variable_kinds: vec![], hints: HintStorage::new(), ctx: MIRBlockContext::new() }
 	}
 
 	pub fn append(&mut self, ctx: &MIRContext, instruction: MIRInstruction) -> InstructionValue {
