b61dd6436501814d6103ead7f54cd210291aa285
Zffu <103074097+Zffu@users.noreply.github.com>
2026-03-17 01:19:16 +0100

    feat: added leftover elements in body member

--- compiler/astoir_mir_lowering/src/body.rs
+++ compiler/astoir_mir_lowering/src/body.rs
@@ -2,11 +2,12 @@ use astoir_hir::nodes::HIRNode;
 use astoir_mir::blocks::{refer::MIRBlockReference};
 use compiler_errors::{IR_INVALID_NODE_TYPE, MATH_OP_NO_ASSIGN, errs::{BaseResult, base::BaseError}};
 
-use crate::{MIRLoweringContext, math::lower_hir_math_operation, vars::lower_hir_variable_assignment};
+use crate::{MIRLoweringContext, control::{forloop::lower_hir_for_loop, ifstatement::lower_hir_if_statement}, math::lower_hir_math_operation, vars::{lower_hir_variable_assignment, lower_hir_variable_declaration}};
 
 pub fn lower_hir_body_member(block: MIRBlockReference, node: Box<HIRNode>, ctx: &mut MIRLoweringContext) -> BaseResult<bool> {
 	return match *node {
 		HIRNode::VarAssigment { .. } => lower_hir_variable_assignment(block, node, ctx),
+		HIRNode::VarDeclaration { .. } => lower_hir_variable_declaration(block, node, ctx),
 		HIRNode::MathOperation { left: _, right: _, operation: _, assignment } => {
 			if !assignment {
 				return Err(BaseError::err(MATH_OP_NO_ASSIGN!().to_string()))
@@ -16,6 +17,9 @@ pub fn lower_hir_body_member(block: MIRBlockReference, node: Box<HIRNode>, ctx: 
 
 			return Ok(true);
 		},
+
+		HIRNode::ForBlock { .. } => lower_hir_for_loop(block, node, ctx),
+		HIRNode::IfStatement { .. } => lower_hir_if_statement(block, node, ctx),
 
 		_ => return Err(BaseError::err(IR_INVALID_NODE_TYPE!().to_string()))
 	}
