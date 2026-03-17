7a7c2524efede3caae26454dc72fd0fb71a9d976
Zffu <103074097+Zffu@users.noreply.github.com>
2026-03-17 16:26:46 +0100

    feat: added LLVMSiblingObject

--- compiler/llvm_ir_bridge/src/utils.rs
+++ compiler/llvm_ir_bridge/src/utils.rs
@@ -1,10 +1,30 @@
 use std::{mem::transmute, ops::Deref, rc::Rc};
 
-use inkwell::context::Context;
+use inkwell::{basic_block::BasicBlock, context::Context};
+
+pub type LLVMBlock = LLVMObject<BasicBlock<'static>>;
 
 pub struct LLVMObject<T: Clone> {
 	pub inner: T,
 	pub ctx: Rc<Context>
+}
+
+/// The LLVMObject without a safety reference.
+/// 
+/// # Safety
+/// Using this is only safe when the object it is contained in contains a Context ref
+pub struct LLVMSiblingObject<T: Clone> {
+	pub inner: T
+}
+
+impl<K: Clone> LLVMSiblingObject<K> {
+	pub fn new(inner: K) -> Self {
+		return LLVMSiblingObject { inner: unsafe { transmute(inner)}}
+	}
+
+	pub fn new_ref(inner: &K) -> Self {
+		return LLVMSiblingObject { inner: unsafe { transmute(K::clone(inner)) }}
+	}
 }
 
 impl<K: Clone> LLVMObject<K> {
@@ -22,6 +42,14 @@ impl<K: Clone> LLVMObject<K> {
 }
 
 impl<T: Clone> Deref for LLVMObject<T> {
+	type Target = T;
+
+	fn deref(&self) -> &Self::Target {
+		return &self.inner;
+	}
+}
+
+impl<T: Clone> Deref for LLVMSiblingObject<T> {
 	type Target = T;
 
 	fn deref(&self) -> &Self::Target {
