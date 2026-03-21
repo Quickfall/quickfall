7ba11da744609de5f384e05c59456cd7a3339091
Zffu <103074097+Zffu@users.noreply.github.com>
2026-03-21 14:13:34 +0100

    feat: added SizedType trait to RawType

--- compiler/compiler_typing/src/raw.rs
+++ compiler/compiler_typing/src/raw.rs
@@ -1,6 +1,6 @@
 //! The raw type declarations
 
-use crate::{enums::{RawEnumEntryContainer, RawEnumTypeContainer}, structs::RawStructTypeContainer};
+use crate::{SizedType, enums::{RawEnumEntryContainer, RawEnumTypeContainer}, structs::RawStructTypeContainer};
 
 /// The raw types. Are also named generics
 #[derive(Clone)]
@@ -23,4 +23,35 @@ pub enum RawType {
 	SizedInteger(bool),
 	SizedFloating(bool),
 	SizedFixedPoint(bool)
+}
+
+impl SizedType for RawType {
+	fn get_size(&self, compacted_size: bool) -> usize {
+		match self {
+			RawType::Integer(size, _) => *size,
+			RawType::Floating(size, _) => *size,
+			RawType::FixedPoint(size_a, size_b, _) => *size_a + size_b,
+			RawType::Boolean => {
+				if compacted_size {
+					return 1;
+				}
+
+				return 8;
+			},
+
+			RawType::Pointer => {
+				if cfg!(target_pointer_width = "32") {
+					return 32;
+				} else if cfg!(target_pointer_width = "64") {
+					return 64
+				} else {
+					return 0;
+				}
+			},
+
+			RawType::StaticString => return 0, // TODO: make sure  we don't need this
+
+			_ => return 0
+		}
+	}
 }
