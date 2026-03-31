473a2d03daf578a39dcbc22b2691adcc6731063f
Zffu <103074097+Zffu@users.noreply.github.com>
2026-03-31 12:44:51 +0200

    feat: added spans

--- compiler/diagnostics/src/diagnostic.rs
+++ compiler/diagnostics/src/diagnostic.rs
@@ -1,5 +1,7 @@
 //! The core of diagnostics
 
+use compiler_utils::Position;
+
 pub enum Level {
 	Error,
 	Warning,
@@ -11,6 +13,16 @@ pub struct Diagnostic {
 	pub code: usize,
 	pub message: String,
 
+	pub primary_span: Span,
+	pub secondary_spans: Vec<Span>,
+
 	pub note: Vec<String>,
-	pub help: Vec<String>,
+	pub help: Vec<String>
+}
+
+pub struct Span {
+	pub start: Position,
+	pub end: Position,
+
+	pub label: String
 }
