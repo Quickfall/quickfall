5e1ce36034cf92dca2ddefa5d396ca67294eaf4a
Zffu <103074097+Zffu@users.noreply.github.com>
2026-03-31 22:16:23 +0200

    feat: added info

---
+++ compiler/diagnostics/src/infos.rs
@@ -0,0 +1,11 @@
+macro_rules! declare_info {
+	($expr: ident, $ind: literal, $err: literal) => {	
+		pub const $expr: (usize, &str) = ($ind, $err);
+	}
+}
+
+// Optimizations infos
+
+declare_info!(REDUCED_CALL, 0, "call will be optimized here");
+declare_info!(SSA_VARIABLE, 1, "variable will be treated as a register value");
+declare_info!(LINEARIZATION, 2, "function will be linearized as it is pure recursion");
--- compiler/diagnostics/src/lib.rs
+++ compiler/diagnostics/src/lib.rs
@@ -1,3 +1,4 @@
 pub mod diagnostic;
 pub mod errors;
 pub mod warnings;
+pub mod infos;
