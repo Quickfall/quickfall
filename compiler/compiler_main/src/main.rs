c2bab051cb67accb33f56acdaabb52a79f8b1dcb
Zffu <103074097+Zffu@users.noreply.github.com>
2026-04-21 14:26:57 +0200

    feat: added version display

--- compiler/compiler_main/src/main.rs
+++ compiler/compiler_main/src/main.rs
@@ -1,9 +1,21 @@
 use clap::Parser;
 
-use crate::cli::{CLICommand, Cli};
+use crate::{
+    cli::{CLICommand, Cli},
+    version::{GIT_HASH, VERSION},
+};
 
 pub mod cli;
+pub mod version;
 
 fn main() {
     let cli = Cli::parse();
+
+    match cli.command {
+        CLICommand::Version => {
+            println!("Quickfall v{} (commit {})", VERSION, GIT_HASH);
+        }
+
+        _ => todo!(),
+    }
 }
