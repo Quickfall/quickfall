26b93e35dc6c284cf1a39060b6cc970d5a1081a0
Zffu <103074097+Zffu@users.noreply.github.com>
2026-04-24 11:29:52 +0200

    chore: added constructors to functions

--- compiler/astoir_hir/src/func.rs
+++ compiler/astoir_hir/src/func.rs
@@ -6,7 +6,11 @@ use crate::{ctx::branched::HIRBranchedContext, nodes::HIRNode};
 
 pub type HIRFunctionArgument = (u64, TypeReference);
 
-/// The new way of storing functions
+/// The new way of storing functions in the HIR layer.
+///
+/// # Advantages
+/// This approach allows for us to easily add components like HIRBranchedContexts or impls to existing functions.
+/// Allowing for clean recursion handling
 #[doc = "Experimental: will progressively be propagated"]
 pub struct HIRNewFunction {
     pub return_type: Option<TypeReference>,
@@ -14,4 +18,56 @@ pub struct HIRNewFunction {
 
     pub branched_ctx: Option<HIRBranchedContext>,
     pub implementation: Option<Box<HIRNode>>,
+
+    /// The number of times this function is called, allows for dead code elimination
+    pub usage_count: usize,
+}
+
+impl HIRNewFunction {
+    /// Creates a new shadow function instance.
+    pub fn new_shadow(return_type: Option<TypeReference>, args: Vec<HIRFunctionArgument>) -> Self {
+        HIRNewFunction {
+            return_type,
+            arguments: args,
+            branched_ctx: None,
+            implementation: None,
+            usage_count: 0,
+        }
+    }
+
+    /// Creates a function that includes basic information (eg return type, arguments) and an HIR branched ctx
+    pub fn new_half(
+        return_type: Option<TypeReference>,
+        args: Vec<HIRFunctionArgument>,
+        branched_ctx: HIRBranchedContext,
+    ) -> Self {
+        HIRNewFunction {
+            return_type,
+            arguments: args,
+            branched_ctx: Some(branched_ctx),
+            implementation: None,
+            usage_count: 0,
+        }
+    }
+
+    /// Creates a function with every information:
+    /// - Basic information
+    /// - Branched CTX
+    /// - Implementation Node
+    ///
+    /// This is usually for late-stage implementation registration.
+    pub fn new_full(
+        return_type: Option<TypeReference>,
+        args: Vec<HIRFunctionArgument>,
+        branched_ctx: HIRBranchedContext,
+        implementation: Box<HIRNode>,
+    ) -> Self {
+        HIRNewFunction {
+            return_type,
+            arguments: args,
+            branched_ctx: Some(branched_ctx),
+            implementation: Some(implementation),
+            usage_count: 0,
+        }
+    }
 }
