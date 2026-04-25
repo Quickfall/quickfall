165f76f59b1d9d5d1e4336fe81b13ee749522d7a
Zffu <103074097+Zffu@users.noreply.github.com>
2026-04-25 18:38:35 +0200

    feat: added new typing crate

--- compiler/astoir_hir/src/lib.rs
+++
@@ -1,9 +0,0 @@
-//! The HIR layer of the AstoIR.
-//! The HIR layer represents a near AST where elements are resolved instead of raw name hashes, enforcing that the code works in theory.
-
-pub mod ctx;
-pub mod func;
-pub mod nodes;
-pub mod resolve;
-pub mod scope;
-pub mod structs;
