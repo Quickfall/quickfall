85acad18f23d2a1eb0798b0c1021eaf26a5edb86
Zffu <103074097+Zffu@users.noreply.github.com>
2026-05-01 18:20:28 +0200

    feat: added StructuredInit -> StructInitTyped conversion

--- compiler/astoir_hir/src/casting.rs
+++ compiler/astoir_hir/src/casting.rs
@@ -1,4 +1,9 @@
-use diagnostics::{DiagnosticResult, DiagnosticSpanOrigin, builders::make_expected_simple_error};
+use diagnostics::{
+    DiagnosticResult, DiagnosticSpanOrigin,
+    builders::{
+        make_expected_simple_error, make_field_no_such, make_field_type, make_struct_missing_field,
+    },
+};
 use typing::{FieldMethodType, container::Type};
 
 use crate::{
@@ -37,8 +42,39 @@ impl HIRNode {
         if self.is_abstract() {
             if let HIRNodeKind::StructuredInit { fields } = self.kind.clone() {
                 for field in t.get_fields() {
-					if !fields.contains_key(k)
-				}
+                    if !fields.contains_key(&field.0) {
+                        return Err(make_struct_missing_field(origin, &t, &field.0.val).into());
+                    }
+
+                    if fields[&field.0]
+                        .get_type(context, func_entry, origin)?
+                        .unwrap()
+                        != field.1
+                    {
+                        return Err(make_field_type(origin, &field.0.val, &field.1).into());
+                    }
+                }
+
+                for field in &fields {
+                    if !t.has_field(
+                        field.0.val.clone(),
+                        field.1.get_type(context, func_entry, origin)?.unwrap(),
+                    ) {
+                        return Err(make_field_no_such(origin, &field.0.val).into());
+                    }
+                }
+
+                let mut f = vec![];
+
+                for field in t.get_fields() {
+                    f.push(fields[&field.0].clone())
+                }
+
+                return Ok(Box::new(HIRNode::new(
+                    HIRNodeKind::StructInitTyped { t, fields: f },
+                    &self.start,
+                    &self.end,
+                )));
             }
 
             panic!("Invalid node")
