ccc7025f125a12ab88a97b334e4af3afca72d250
Zffu <103074097+Zffu@users.noreply.github.com>
2026-05-07 09:12:01 +0200

    feat: added llvm IR build

--- compiler/compiler_main/src/cmds/build.rs
+++ compiler/compiler_main/src/cmds/build.rs
@@ -4,6 +4,9 @@ use ast_parser::parse_ast_ctx;
 use astoir::run_astoir_mir;
 use lexer::lexer::lexer_parse_file;
 
+#[cfg(feature = "llvm")]
+use llvm_ir_bridge::bridge_llvm;
+
 use crate::quietlyquit_if_errors;
 
 pub fn build_mir(path: String, out: PathBuf) {
@@ -17,4 +20,25 @@ pub fn build_mir(path: String, out: PathBuf) {
     quietlyquit_if_errors!();
 
     fs::write(out, format!("{}", mir.unwrap())).unwrap();
+}
+
+#[cfg(feature = "llvm")]
+pub fn build_llvm(path: String, out: PathBuf) {
+    let lexer = lexer_parse_file(&path);
+    quietlyquit_if_errors!();
+
+    let ast = parse_ast_ctx(&lexer.unwrap());
+    quietlyquit_if_errors!();
+
+    let mir = run_astoir_mir(ast.unwrap());
+    quietlyquit_if_errors!();
+
+    let llvm = bridge_llvm(&mir.unwrap());
+
+    llvm.module.print_to_file(out).unwrap();
+}
+
+#[cfg(not(feature = "llvm"))]
+pub fn build_llvm(_path: String, _out: PathBuf) {
+    println!("LLVM bridge is disabled in this version of the compiler.")
 }
